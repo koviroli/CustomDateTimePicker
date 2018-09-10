f82fdb7be4baa654e790d9bdfaecabba4e9dbb2c
Kovacs Roland <kovacs.roland.labtech@gmail.com>
2018-09-10 09:23:39 +0200

    still removing not used files and calsses

--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -12,43 +12,9 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        ECultureDateFormat dateFormat;
-        string OldText = string.Empty;
-
         public Form1()
         {
             InitializeComponent();
-          
-            dateFormat = ECultureDateFormat.ddmmyyyy;
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ToolStripDropDown tsdd = new ToolStripDropDown();
-            tsdd.Show(250, 250);
-        }
-        private List<int> GetIndexesOf(string s, char ch)
-        {
-            var foundIndexes = new List<int>();
-
-            long t1 = DateTime.Now.Ticks;
-            for (int i = s.IndexOf(ch); i > -1; i = s.IndexOf(ch, i + 1))
-            {
-                // for loop end when i=-1 ('a' not found)
-                foundIndexes.Add(i);
-            }
-            return foundIndexes;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
         }
     }
 }
